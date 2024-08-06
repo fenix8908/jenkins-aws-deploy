3da88ffbaacbd8dd4007860195013f9b76790e9c
JORGE <marialuci1019@gmail.com>
2024-08-06 09:52:39 -0500

    subiendo el proyecto

---
+++ src/test/java/co/com/devops/deploy1/Deploy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.devops.deploy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
